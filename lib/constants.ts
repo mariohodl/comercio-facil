0d6fda1b35522e498225a9e2c5b4c5f06a7cbef3
Mario Godinez <mgodinez@specialized.com>
2025-03-08 07:37:14 -0600

    Cleanup and constant config added

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.APP_NAME || 'Master Carnes';
+export const APP_URL = process.env.APP_URL || 'http://localhost:3000';
+export const APP_DESCRIPTION =
+	process.env.APP_DESCRIPTION ||
+	'Distribuidor de carnes de occidente, el privilegio de..';
+export const APP_SLOGAN = process.env.APP_SLOGAN || 'Come mas, gasta menos';
