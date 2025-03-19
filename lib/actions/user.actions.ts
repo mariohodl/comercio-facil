8dc05073afaf08af281c05544c25ee80b7f66b97
Mario Godinez <mgodinez@specialized.com>
2025-03-19 17:31:39 -0600

    15-register-user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,12 @@
 'use server';
+import bcrypt from 'bcryptjs';
 import { signIn, signOut } from '@/auth';
+import { IUserSignIn, IUserSignUp } from '@/types';
+import { UserSignUpSchema } from '../validator';
+import { connectToDatabase } from '../db';
+import User from '../db/models/user.model';
+import { formatError } from '../utils';
 import { redirect } from 'next/navigation';
-import { IUserSignIn } from '@/types';
 
 export async function signInWithCredentials(user: IUserSignIn) {
 	return await signIn('credentials', { ...user, redirect: false });
@@ -11,3 +16,24 @@ export const SignOut = async () => {
 
 	redirect(redirectTo.redirect);
 };
+
+// CREATE
+export async function registerUser(userSignUp: IUserSignUp) {
+	try {
+		const user = await UserSignUpSchema.parseAsync({
+			name: userSignUp.name,
+			email: userSignUp.email,
+			password: userSignUp.password,
+			confirmPassword: userSignUp.confirmPassword,
+		});
+
+		await connectToDatabase();
+		await User.create({
+			...user,
+			password: await bcrypt.hash(user.password, 5),
+		});
+		return { success: true, message: 'User created successfully' };
+	} catch (error) {
+		return { success: false, error: formatError(error) };
+	}
+}
