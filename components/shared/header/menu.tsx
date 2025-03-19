13295d929c91589a75ec414a10fa7cc658156b01
Mario Godinez <mgodinez@specialized.com>
2025-03-19 10:42:53 -0600

    14-signin-user

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link'
 import CartButton from './cart-button'
+import UserButton from './user-button'
 
 export default function Menu() {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
-        <Link href='/cart' className='header-button'>
-          <span className='font-bold'>Sign in</span>
-        </Link>
+        <UserButton />
 
         <CartButton />
       </nav>
