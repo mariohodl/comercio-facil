ad67616c214598db1ab70a59b082316bd6ac3307
Mario Godinez <mgodinez@specialized.com>
2025-03-27 16:29:32 -0600

    16-signin-with-google

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 import { auth } from '@/auth'
+import { GoogleSignInForm } from './google-signin-form'
 import SeparatorWithOr from '@/components/shared/separator-or'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
@@ -37,6 +38,10 @@ export default async function SignIn(props: {
         <CardContent>
           <div>
             <CredentialsSignInForm />
+          </div>
+          <SeparatorWithOr />
+          <div className='mt-4'>
+            <GoogleSignInForm />
           </div>
         </CardContent>
       </Card>
