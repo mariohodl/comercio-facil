890dc21dee4511d2baa48951e2dc612f7cb863d4
Mario Godinez <mgodinez@specialized.com>
2025-04-14 22:05:11 -0600

    21-rate-and-review-products

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -11,7 +11,9 @@ import ProductPrice from '@/components/shared/product/product-price'
 import ProductGallery from '@/components/shared/product/product-gallery'
 import { Separator } from '@/components/ui/separator'
 import ProductSlider from '@/components/shared/product/product-slider'
-import Rating from '@/components/shared/product/rating'
+import RatingSummary from '@/components/shared/product/rating-summary'
+import ReviewList from './review-list'
+import { auth } from '@/auth'
 import BrowsingHistoryList from '../../../../components/shared/browsing-history-list';
 import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
 
@@ -49,6 +51,8 @@ export default async function ProductDetails(props: {
     page: Number(page || '1'),
   })
 
+  const session = await auth()
+
   return (
     <div>
       <AddToBrowsingHistory id={product._id} category={product.category} />
@@ -66,11 +70,12 @@ export default async function ProductDetails(props: {
               <h1 className='font-bold text-lg lg:text-xl'>
                 {product.name}
               </h1>
-              <div className='flex items-center gap-2'>
-                <span>{product.avgRating.toFixed(1)}</span>
-                <Rating rating={product.avgRating} />
-                <span>{product.numReviews} ratings</span>
-              </div>
+              <RatingSummary
+                avgRating={product.avgRating}
+                numReviews={product.numReviews}
+                asPopover
+                ratingDistribution={product.ratingDistribution}
+              />
               <Separator />
               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
                 <div className='flex gap-3'>
@@ -139,6 +144,13 @@ export default async function ProductDetails(props: {
             
           </div>
         </div>
+      </section>
+
+      <section className='mt-10'>
+        <h2 className='h2-bold mb-2' id='reviews'>
+          Customer Reviews
+        </h2>
+        <ReviewList product={product} userId={session?.user.id} />
       </section>
 
       <section className='mt-10'>
