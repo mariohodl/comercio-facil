d9a7675c593379f83b6423d420da96815e19dcae
Mario Godinez <mgodinez@specialized.com>
2025-04-23 09:42:00 -0600

    30-admin-orders

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,7 @@ const config: Config = withUt({
 			},
 		},
 	},
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	plugins: [require('tailwindcss-animate')],
 }) satisfies Config;
 
