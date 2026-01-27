e4b3fbabe1c4a1094a534c53ff03e23dcc2b9684
Mario <mariohodl21@gmail.com>
2026-01-27 10:59:36 -0600

    Fixed auth social issue, vercel/NextAuth recommended pattern of exporting a clientPromise

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -1,4 +1,3 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
@@ -15,23 +14,24 @@ const options = {
 };
 
 let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
-	// In development mode, use a global variable so that the value
-	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	// In development mode, use a global variable so that the value is preserved across module reloads caused by HMR (Hot Module Replacement).
 	const globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient;
+		_mongoClientPromise?: Promise<MongoClient>;
 	};
 
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options);
+	if (!globalWithMongo._mongoClientPromise) {
+		client = new MongoClient(uri, options);
+		globalWithMongo._mongoClientPromise = client.connect();
 	}
-	client = globalWithMongo._mongoClient;
+	clientPromise = globalWithMongo._mongoClientPromise;
 } else {
 	// In production mode, it's best to not use a global variable.
 	client = new MongoClient(uri, options);
+	clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
-export default client;
+// Export a module-scoped MongoClient promise. By doing this in a separate module, the client can be shared across functions.
+export default clientPromise;
