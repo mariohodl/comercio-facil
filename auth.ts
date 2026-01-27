a62eb9234312b2b389ac0d8543c8c3276a5766b4
Mario <mariohodl21@gmail.com>
2026-01-27 11:28:07 -0600

    Updated condig auth.ts to ensure facebook login issues fixed

--- auth.ts
+++ auth.ts
@@ -31,6 +31,7 @@ declare module 'next-auth' {
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	...authConfig,
+	trustHost: true,
 	session: {
 		strategy: 'jwt',
 		maxAge: 30 * 24 * 60 * 60,
@@ -41,9 +42,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 			allowDangerousEmailAccountLinking: true,
 		}),
 		Facebook({
+			clientId: process.env.AUTH_FACEBOOK_ID,
+			clientSecret: process.env.AUTH_FACEBOOK_SECRET,
 			allowDangerousEmailAccountLinking: true,
 		}),
 		Instagram({
+			clientId: process.env.AUTH_INSTAGRAM_ID,
+			clientSecret: process.env.AUTH_INSTAGRAM_SECRET,
 			allowDangerousEmailAccountLinking: true,
 		}),
 		CredentialsProvider({
